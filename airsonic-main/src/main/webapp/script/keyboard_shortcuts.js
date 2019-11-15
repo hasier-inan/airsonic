8eb02a59c996990e56b508ba86bdfdc223fc201c
Hasier Iñan <hasiermetal@gmail.com>
2019-11-15 10:33:16 +0100

    Updated home shortcut.

--- airsonic-main/src/main/webapp/script/keyboard_shortcuts.js
+++ airsonic-main/src/main/webapp/script/keyboard_shortcuts.js
@@ -19,7 +19,7 @@ if (isKeyboardShortcutsEnabled()) {
   Mousetrap.bind('/',     function() { parent.frames.upper.$("#query").focus(); });
   Mousetrap.bind('m',     function() { parent.frames.upper.toggleLeftFrameVisible(); });
 
-  Mousetrap.bind('g h', function() { parent.frames.main.location.href = "home.view?"; });
+  Mousetrap.bind('h', function() { parent.frames.main.location.href = "home.view?"; });
   Mousetrap.bind('g p', function() { parent.frames.main.location.href = "playlists.view?"; });
   Mousetrap.bind('g o', function() { parent.frames.main.location.href = "podcastChannels.view?"; });
   Mousetrap.bind('g s', function() { parent.frames.main.location.href = "settings.view?"; });
