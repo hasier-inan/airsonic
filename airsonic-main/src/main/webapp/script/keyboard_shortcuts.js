92ab59b466153a606a64c1deb2faaf3e67d565c5
Hasier Iñan <hasiermetal@gmail.com>
2019-11-23 21:10:54 +0100

    Added more shortcuts

--- airsonic-main/src/main/webapp/script/keyboard_shortcuts.js
+++ airsonic-main/src/main/webapp/script/keyboard_shortcuts.js
@@ -9,17 +9,25 @@ function isKeyboardShortcutsEnabled() {
 
 if (isKeyboardShortcutsEnabled()) {
   Mousetrap.bind('space', function() { parent.frames.playQueue.onToggleStartStop(); return false; });
+  Mousetrap.bind('s', function() { parent.frames.playQueue.onToggleStartStop(); return false; });
   Mousetrap.bind('left',  function() { parent.frames.playQueue.onPrevious(); });
+  Mousetrap.bind('a',  function() { parent.frames.playQueue.onPrevious(); });
   Mousetrap.bind('right', function() { parent.frames.playQueue.onNext(); });
+  Mousetrap.bind('d', function() { parent.frames.playQueue.onNext(); });
   Mousetrap.bind('*',     function() { parent.frames.playQueue.onStarCurrent(); });
   Mousetrap.bind('plus',  function() { parent.frames.playQueue.onGainAdd(+5); });
+  Mousetrap.bind('r',  function() { parent.frames.playQueue.onGainAdd(+5); });
   Mousetrap.bind('-',     function() { parent.frames.playQueue.onGainAdd(-5); });
+  Mousetrap.bind('f',     function() { parent.frames.playQueue.onGainAdd(-5); });
   Mousetrap.bind('q',     function() { parent.frames.playQueue.onTogglePlayQueue(); });
 
   Mousetrap.bind('/',     function() { parent.frames.upper.$("#query").focus(); });
   Mousetrap.bind('m',     function() { parent.frames.upper.toggleLeftFrameVisible(); });
+  Mousetrap.bind('e',     function() { parent.frames.upper.toggleLeftFrameVisible(); });
 
   Mousetrap.bind('h', function() { parent.frames.main.location.href = "home.view?"; });
+  Mousetrap.bind('1', function() { parent.frames.main.location.href = "home.view?"; });
+  Mousetrap.bind('2', function() { parent.frames.main.location.href = "nowPlaying.view?"; });
   Mousetrap.bind('g p', function() { parent.frames.main.location.href = "playlists.view?"; });
   Mousetrap.bind('g o', function() { parent.frames.main.location.href = "podcastChannels.view?"; });
   Mousetrap.bind('g s', function() { parent.frames.main.location.href = "settings.view?"; });
